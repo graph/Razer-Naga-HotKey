6c3c7969d8f3a176d1d54d4ccf085debcef881e5
Dan Panzarella <pzl@users.noreply.github.com>
2015-02-06 14:03:45 -0500

    be more lenient in accepting Naga titles
    "Razer Naga 2014" is apparently a new string used to identify this device. Was getting ruled out by the regex here.

--- naga_disablekbd.pl
+++ naga_disablekbd.pl
@@ -8,7 +8,7 @@ my @lines = split("\n", $linesString);
 
 for(my $i =0; $i < @lines; $i++){
 	my $l = $lines[$i];
-	if($l =~ /Razer Naga\s+id=(\d+).+keyboard/c){
+	if($l =~ /Razer Naga.*id=(\d+).+keyboard/c){
 		# found the keyboard input
 		my $id = $1;
 		system("xinput set-int-prop $id \"Device Enabled\" 8 0");
